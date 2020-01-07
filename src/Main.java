e4cb08678b0eeae5ed7c7905c26bf6df3c52f1b3
João Henrique Xavier <jhcxavier@gmail.com>
2020-01-06 21:14:51 -0500

    manipulating string

--- src/Main.java
+++ src/Main.java
@@ -3,20 +3,15 @@ import java.awt.*;
 public class Main{
 
     public static void main(String[] args){
-        System.out.println("Hello World");
-        System.out.println("Joao");
+        String userInput = "entertainment";
+        String uppercased = userInput.toUpperCase();
+        System.out.println(userInput);
+        System.out.println(uppercased);
 
-        Car myCar = new Car(25.5,
-                            "1BC453",
-                             Color.BLUE, true);
-        Car sallyCar = new Car(13.9,
-                "35DG45",
-                Color.BLACK,
-                false );
-        System.out.println("My car is License Plate: " + myCar.licensePlate);
-        System.out.println("My car is License Plate: " + sallyCar.licensePlate);
-        System.out.println(myCar.paintColor.toString());
-        myCar.changePaintColor(Color.RED);
-        System.out.println(myCar.paintColor.toString());
+        char firstCharater = uppercased.charAt(0);
+        System.out.println(firstCharater);
+
+        System.out.println("Contains: " +
+                userInput.contains("Enter".toLowerCase()));
     }
 }
