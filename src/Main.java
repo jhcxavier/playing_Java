0a2beb9abeadad1d4375e52fe86eee29e3fdc45a
João Henrique Xavier <jhcxavier@gmail.com>
2020-01-13 16:25:11 -0500

    updates

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,18 @@
 import java.awt.*;
+import java.util.Scanner;
 
 public class Main{
 
     public static void main(String[] args){
-        String userInput = "entertainment";
+
+      // System.out.println("Enter a word:");
+        Scanner sc = new Scanner(System.in);
+        int userNumber = sc.nextInt();
+        System.out.println(userNumber);
+        double userNumber2 = sc.nextDouble();
+        System.out.println(userNumber2);
+/*
+        String userInput = sc.next();
         String uppercased = userInput.toUpperCase();
         System.out.println(userInput);
         System.out.println(uppercased);
@@ -13,5 +22,7 @@ public class Main{
 
         System.out.println("Contains: " +
                 userInput.contains("Enter".toLowerCase()));
+                */
+
     }
 }
